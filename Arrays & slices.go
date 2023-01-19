e1c22334aeb91092cc1add2c96673d6209a2c881
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-19 16:32:12 +0000

    Create Array & slices.go

---
+++ Arrays & slices.go
@@ -0,0 +1,20 @@
+// You can edit this code!
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	grade1 := 97
+	grade2 := 93
+	grade3 := 92
+	fmt.Printf("Grades:%v,%v,%v", grade1, grade2, grade3)
+}
+
+
+//now for the array 
+
+func main() {
+  grades :=[3]int{97,85,93 }
+  fmt.Printf("Grades: %v" , grades)
