acdc51e2f3aed882a26bfa7a41dfc128b9151833
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-19 17:22:16 +0000

    Update Arrays & slices.go

--- Arrays & slices.go
+++ Arrays & slices.go
@@ -35,3 +35,35 @@ func main() {
 	fmt.Printf("Students: %v\n", students[2])
 	fmt.Printf("No of Students: %v\n", len(students))
 }
+
+
+
+// making an array of an array
+func main() {
+	var identityMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
+	fmt.Println(identityMatrix)
+}
+
+func main() {
+	var identifyMatrix [3][3]int
+	identifyMatrix[0] = [3]int{1,0,0}
+	identifyMatrix[1] = [3]int{0,1,0}
+	identifyMatrix[2] = [3]int{0,0,1}
+	fmt.Println(identifyMatrix)
+}
+
+
+func main() {
+	a:= [...]int{1,2,3}
+	b := a
+	b[1] = 5
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+
+
+
+
+
+
