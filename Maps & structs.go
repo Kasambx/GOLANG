483481444cb94197f6551be389508c944d856d51
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-23 14:25:42 +0000

    Update Maps & structs.go

--- Maps & structs.go
+++ Maps & structs.go
@@ -31,4 +31,30 @@ func main() {
 
 
 // STRUCS 
+package main
+
+import (
+	"fmt"
+)
+
+type Doctor struct {
+	number     int
+	actorName  string
+	companions []string
+}
+
+func main() {
+	aDoctor := Doctor{
+		number:    3,
+		actorName: "Jon Pertwee",
+		companions: []string{
+			"Liz Shaw",
+			"Jo Grant",
+			"Sarah Jane Smith",
+		},
+	}
+	fmt.Println(aDoctor)
+
+}
+
 
