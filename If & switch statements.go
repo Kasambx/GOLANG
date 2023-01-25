5ac26fb1d06fd385b5e7e4b75857b37a5e612ac8
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-25 03:08:45 +0000

    Update If & switch statements.go

--- If & switch statements.go
+++ If & switch statements.go
@@ -11,4 +11,53 @@ func main() {
 	}
 }
 
+//using if and pop
+package main
+
+import "fmt"
+
+func main() {
+	statePopulations := make(map[string]int) //one way to make a map
+	statePopulations = map[string]int{       //the second way to make a map
+		"California":   39260017,
+		"Texas":        27862596,
+		"Florida":      20612439,
+		"New York":     19745289,
+		"Pennsylvania": 12802503,
+		"Illinois":     12801539,
+		"Ohio":         11614373,
+	}
+
+	if pop, ok := statePopulations["New York"]; ok { //shows whether the element is present and its value
+		fmt.Println(pop)
+	}
+
+}
+
+//another example
+package main
+
+import "fmt"
+
+func main() {
+	number := 50
+	guess := 30
+	if guess < number {
+		fmt.Println("Too Low")
+
+	}
+	if guess > number {
+		fmt.Println("Too high")
+	}
+	if guess == number {
+		fmt.Println("You got it !")
+	}
+	fmt.Println(number<=guess, number>= guess, number!=guess)  //returns boolean expressions for the variable in the func mamin statement
+}
+
+
+
+
+
+
 
