2a623a25c7292a4ad08f20f08973ee9399125e27
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-30 10:05:33 +0000

    Update defer, panic &recover.go

--- defer, panic &recover.go
+++ defer, panic &recover.go
@@ -26,3 +26,36 @@ func main() {
 	defer fmt.Println(a)
 	a = "end"
 }
+
+
+// the following program prints out panic which shows that program can no longer continue to execute 
+// panic shows that the program cant continue to execute 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a, b := 1, 0
+	ans := a / b
+	fmt.Println(ans)
+}
+
+
+//another example 
+// the following program prints out panic which shows that program can no longer continue to execute
+// panic shows that the program cant continue to execute
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("I'll try")
+	panic("that's not enough ")
+	fmt.Println("I'll do my best")
+}
+
+
