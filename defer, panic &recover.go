3e8df05f2adb5e2204ea7ef06246c0f9ad4db346
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-30 09:45:24 +0000

    Update defer, panic &recover.go

--- defer, panic &recover.go
+++ defer, panic &recover.go
@@ -1,1 +1,13 @@
+// defer function in Go
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Start")
+	defer fmt.Println("Middle")			//defer mves this statement before the main function is returned
+	fmt.Println("End")
+}
