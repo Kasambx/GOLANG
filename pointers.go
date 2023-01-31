fc62d2c23221caf72feda3f6e02a66cbc6e87667
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-01-31 09:31:16 +0000

    Update pointers.go

--- pointers.go
+++ pointers.go
@@ -1,1 +1,19 @@
+//pointers
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 42
+	var b *int = &a     // * points b to address of a
+	fmt.Println(&a, *b) // * pulls the value in the address of B
+	a = 30				//value of b changes after changing value of a
+	fmt.Println(a, *b)
+	*b = 14
+	fmt.Println(a, *b)		//value of a changes after changing value of b
+}
+
 
