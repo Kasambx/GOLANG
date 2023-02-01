de535324213749f6d2d8c505e17509bb78703f55
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-01 10:49:02 +0000

    Update functions.go

--- functions.go
+++ functions.go
@@ -228,6 +228,30 @@ func main() {
 }
 
 
+// annonymous function
+//another one a bit more complex
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var divide func(float64, float64) (float64, error)
+	divide = func(a, b float64) (float64, error) {
+		if b == 0.0 {
+			return 0.0, fmt.Errorf("Cannot divide by zero")
+		} else {
+			return a / b, nil
+		}
+	}
+	d, err := divide(5.0, 3.0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(d)
+}
 
 
 
