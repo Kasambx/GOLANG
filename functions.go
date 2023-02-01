d9819ac3a42fc5314e432b7bc030207184aaa128
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-01 08:54:12 +0000

    Update functions.go

--- functions.go
+++ functions.go
@@ -7,3 +7,23 @@ import "fmt"
 func main () {
   fmt.Println("Hey I am the best")
 }
+
+
+// defining a function that takes parameters from another function
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sayMessage("Hello Go!")
+}
+
+func sayMessage(msg string) {
+	fmt.Println(msg)
+}
+
+
+
+
