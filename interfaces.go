24845d1f1d747708501b9449c974f32e05fc3afc
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-01 17:59:36 +0000

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,5 @@
 //interfaces in go 
-
+//using struct
 
 package main
 
@@ -22,3 +22,59 @@ func (cw ConsoleWriter) Write(data []byte) (int, error) {
 	return n, err
 }
 
+
+//single method interface
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
+}
+
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
+
+//interfaces in interfaces ***** do some research*****
+// type conversions in interfaces 
+// do some reseaarch on empty interfaces 
+
+
+//type switches 
+//interface switching
+//interfaces in go
+//interface switching
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i interface{} = "weuwe"
+	switch i.(type) { //this is called a type switch
+	case int:
+		fmt.Println("i is an integer")
+	case string:
+		fmt.Println("i is a string")
+	default:
+		fmt.Println("I dont know what i is")
+	}
+}
