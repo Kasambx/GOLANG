74a8298bddf10920f9004fe487a27ee5cfbd6af4
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-01 17:30:54 +0000

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -1,1 +1,24 @@
+//interfaces in go 
+
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
 
