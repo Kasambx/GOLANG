a8f0b05e55214557f820dcc7183af987994b9366
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-01 11:07:44 +0000

    Update functions.go

--- functions.go
+++ functions.go
@@ -255,6 +255,34 @@ func main() {
 
 
 
+//methods 
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	g := greeter{
+		greeting: "Hello",
+		name:     "Go",
+	}
+	g.greet()
+
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g greeter) greet() {
+	fmt.Println(g.greeting, g.name)
+
+}
+
+
 
 
 
