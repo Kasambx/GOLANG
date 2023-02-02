57bdfae68988ca86b1f77c509a605b4616c58412
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-02-02 11:32:42 +0000

    Update routines.go

--- routines.go
+++ routines.go
@@ -1,16 +1,17 @@
-// go routinef
+// go routines
 
 package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	sayHello()
+	go sayHello()                      // the go is used to make a goroutine //n/b nothing will be displayed after printing
+	time.Sleep(100 * time.Millisecond) //put some delay so that the print statement can be displayed
 }
 
 func sayHello() {
-	fmt.Println("Hello	f")
+	fmt.Println("Hello")
 }
-
